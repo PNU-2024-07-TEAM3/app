ff3a2f70d3edd6f1c73c99cb42f2ba2dd0cb972e
kllerredstark <redstars1022@gmail.com>
2024-08-06 15:45:09 +0900

    충돌병합

--- src/main/java/com/ll/sapp/studyroom/StudyRoomController.java
+++ src/main/java/com/ll/sapp/studyroom/StudyRoomController.java
@@ -4,8 +4,10 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -15,14 +17,21 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 public class StudyRoomController {
-    private final StudyRoomRepository studyRoomRepository;
+    private final StudyRoomService studyRoomService;
 
-    @GetMapping("classup/list")
+    @GetMapping("studyroom/list")
     public String list(Model model) {
 
-        List<StudyRoom> StudyRoomList = this.studyRoomRepository.findAll();
+        List<StudyRoom> StudyRoomList = this.studyRoomService.getList();
         model.addAttribute("StudyRoomList", StudyRoomList);
         return "studyroom_list";
         }
+
+    @GetMapping(value = "/studyroom/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "studyroom_detail";
     }
+    }
+
+
 
--- src/main/java/com/ll/sapp/studyroom/StudyRoomService.java
+++ src/main/java/com/ll/sapp/studyroom/StudyRoomService.java
@@ -1,21 +1,32 @@
 package com.ll.sapp.studyroom;
 
+import ch.qos.logback.classic.encoder.JsonEncoder;
 import com.ll.sapp.user.SiteUser;
+import com.ll.sapp.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Store;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
 public class StudyRoomService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
     // 스터디룸 개설
-    public StudyRoom create(String title, ) {
-        SiteUser user = new SiteUser();
-        user.setUserName(username);
-        user.setNickname(nickname);
-        user.setPassword(passwordEncoder.encode(password));
-        this.userRepository.save(user);
-        return user;
+    public StudyRoom create(String username, String nickname, String password) {
+        return null;
+    }
+
+    private final StudyRoomRepository studyRoomRepository;
+
+    public List<StudyRoom> getList() {
+        return this.studyRoomRepository.findAll();
     }
 }
