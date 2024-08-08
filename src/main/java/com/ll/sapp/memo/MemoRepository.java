4af08952b5fe289cf223652bf3400f61c75ab95c
ssong <98401648+hardjava@users.noreply.github.com>
2024-08-08 16:23:44 +0900

    fixMemofunc (#19)

--- src/main/java/com/ll/sapp/memo/MemoRepository.java
+++ src/main/java/com/ll/sapp/memo/MemoRepository.java
@@ -10,6 +10,10 @@ import java.util.List;
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     boolean existsByUser(SiteUser user);
+
     List<Memo> findByDailyStudy(DailyStudy dailyStudy);
+
+    // 새로운 메서드 추가
+    boolean existsByDailyStudyAndUser(DailyStudy dailyStudy, SiteUser user);
 }
 
