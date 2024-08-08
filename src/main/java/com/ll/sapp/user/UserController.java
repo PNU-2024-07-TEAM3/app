20208b0e6e78b3137ba45e7513273dc6006557af
Song <fkdlcn123@naver.com>
2024-08-08 14:31:14 +0900

    ãnav 에 현재 접속중인 user의 nickname이 표시

--- src/main/java/com/ll/sapp/user/UserController.java
+++ src/main/java/com/ll/sapp/user/UserController.java
@@ -4,10 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 
 @RequiredArgsConstructor
